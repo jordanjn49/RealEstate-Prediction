a6dbf7a26b55538549e4c3e06a913b4c8d45f240
jordanjouin@gmail.com <jordanjouin@gmail.com>
2022-12-14 15:21:52 +0100

    dataset density

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import data_graphic
 import data_process
 
 if __name__ == "__main__":
-
     # We read and filter columns from dataset file
     # dataset = data_process.preprocessing('DATASET-VF2022S1.txt')
 
